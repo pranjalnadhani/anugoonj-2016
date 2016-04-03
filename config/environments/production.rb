b66dcd4e0e7fe60b33ca0aca51eee5a9e8d1f25c
Pranjal Nadhani <pranjalnadhani@gmail.com>
2016-04-03 21:55:59 +0530

    rails g devise:install (with necessary changes).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,11 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  # Reuired to change font.
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
